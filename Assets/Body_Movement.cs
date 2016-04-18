f597080a143b3f95d23dfc4826e94355a2208f3d
Kevin Hernandez <hernandez.353@osu.edu>
2016-04-18 15:57:15 -0400

    Messing around with water

---
+++ Assets/Body_Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Body_Movement : MonoBehaviour {
+
+	public float move_speed = 0.1f;
+	public float rotate_speed = 2.0f;
+
+	//cube-plane determination
+	private bool in_neg_x_wall = false; //closest to origin (0)
+	private bool in_pos_x_wall = false; //closest to edge (around 3)
+	private bool in_neg_z_wall = false; //closest to edge (behind starting postion)
+	private bool in_pos_z_wall = false; //cloest to origin (0)
+	public float cube_buffer = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow))
+			transform.Translate (0, 0, 1 * move_speed);
+		if (Input.GetKey (KeyCode.DownArrow))
+			transform.Translate (0, 0, -1 * move_speed);
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Rotate (0, -1 * rotate_speed, 0);
+		if (Input.GetKey (KeyCode.RightArrow))
+			transform.Rotate (0, 1 * rotate_speed, 0);
+
+		//player postion
+		float p_x = transform.position.x;
+		float p_z = transform.position.z;
+	}
+}
---
+++ Assets/Camera.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public float rotation_speed = 2.0f;
+
+	private float swing_angle = 0.0f;
+	private float elevate_angle = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButton (0)) {
+			transform.localRotation = Quaternion.identity;
+
+			float swing_request = Input.GetAxis ("Mouse Y") * rotation_speed;
+			float elevate_request = Input.GetAxis ("Mouse X") * rotation_speed;
+
+			swing_angle += swing_request;
+			elevate_angle += elevate_request;
+			
+			transform.Rotate (-swing_angle, elevate_angle, 0);
+
+			print ("X: " + transform.localEulerAngles.x + ", swing_angle: " + swing_angle + " Z: " + transform.localEulerAngles.z);
+			print ("Y: " + transform.localEulerAngles.y + ", elevation_angle: " + elevate_angle + " Z: " + transform.localEulerAngles.z);
+
+		} else {
+			transform.localRotation = Quaternion.identity;
+			if (elevate_angle < 0)
+				elevate_angle += 1f;
+			if (elevate_angle > 0)
+				elevate_angle -= 1f;
+			if (swing_angle < 0)
+				swing_angle += 1f;
+			if (swing_angle > 0)
+				swing_angle -= 1f;
+
+			//float temp_swing = swing_angle * -1 * .05f;
+			//float temp_elevation = elevate_angle * -1 *.05f;
+
+			//transform.Rotate (temp_swing, temp_elevation, 0);
+			transform.Rotate (-swing_angle, elevate_angle, 0);
+
+			//swing_angle += temp_swing;
+			//elevate_angle += temp_elevation;
+
+			if (Mathf.Abs (swing_angle) < 1 && Mathf.Abs (elevate_angle) < 1) {
+				transform.localRotation = Quaternion.identity;
+				swing_angle = 0;
+				elevate_angle = 0;
+			}
+
+		}
+	
+	}
+}
