dfe2457bf9c7fa342992dd6dce58df3b9e4edcea
Kevin Hernandez <hernandez.353@osu.edu>
2016-04-21 12:14:44 -0400

    Tried to split the cannons up so we could have separate Player1 and Player2

---
+++ Assets/Scripts/Player1/Cannon1_Controls.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon1_Controls : MonoBehaviour {
+
+	//maximum amount of cannon rotation
+	public float MAX_DOWN_ANGLE = 10f;
+	public float MAX_UP_ANGLE = 20f;
+
+
+	//cannon rotation speed
+
+	public float UP_DOWN_ROTATION_SPEED = 1.0f;
+
+	//markers for how much rotation has occured
+	public float downRotation;
+	public float upRotation;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//initialize rotation values
+		downRotation = 0f;
+		upRotation = 0f;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		print (Player1_Control.isActive);
+		if (Player1_Control.isActive) {
+			//down rotation
+			if (Input.GetKey (KeyCode.S) && downRotation < MAX_DOWN_ANGLE) {
+				transform.Rotate (1 * UP_DOWN_ROTATION_SPEED, 0, 0);
+				downRotation += UP_DOWN_ROTATION_SPEED;
+				upRotation -= UP_DOWN_ROTATION_SPEED;
+			}
+			//up rotation
+			if (Input.GetKey (KeyCode.W) && upRotation < MAX_UP_ANGLE) {
+				transform.Rotate (-1 * UP_DOWN_ROTATION_SPEED, 0, 0);
+				upRotation += UP_DOWN_ROTATION_SPEED;
+				downRotation -= UP_DOWN_ROTATION_SPEED;
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Player2/FireCannon2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCannon2 : MonoBehaviour {
+
+	private GameObject cannonBall;
+	public float speed;
+	private float interval;
+	public GameObject ball;
+	// Use this for initialization
+	void Start () {
+		speed = 100.0f;
+		interval = 1.015f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (Player2_Control.isActive) {
+			if (Input.GetKey (KeyCode.Space)) {
+				speed *= interval;
+				if (speed > 10000.0f) {
+					interval = 0.995f;
+				}
+				if (speed < 100.0f) {
+					interval = 1.015f;
+				}
+				print (speed);
+			}
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				cannonBall = (GameObject)Instantiate (ball, transform.position, transform.rotation);
+				Rigidbody ballDynamics = cannonBall.GetComponent<Rigidbody> ();
+				ballDynamics.AddForce (transform.forward * speed);
+				speed = 100.0f;
+				Player1_Control.isActive = true;
+				Player2_Control.isActive = false;
+			}
+		}
+	}
+}
