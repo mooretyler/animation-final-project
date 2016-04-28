a92b29956400b03eba1052a700a15f34ed749794
Kevin Hernandez <hernandez.353@osu.edu>
2016-04-28 14:53:19 -0400

    Added platform collision detections

---
+++ Assets/Scripts/Player1/PlatformP1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformP1 : MonoBehaviour {
+
+	void OnTriggerEnter (Collider cannonball){
+		if(cannonball.gameObject.name == "CannonBallp2(Clone)"){
+			GameObject p1Camera = GameObject.Find ("P1Camera");
+			GameObject p2Camera = GameObject.Find ("P2Camera");
+			p1Camera.GetComponent<UnityEngine.Camera> ().enabled = !Player1_Control.isActive;
+			p2Camera.GetComponent<UnityEngine.Camera> ().enabled = !Player2_Control.isActive;
+			Destroy (cannonball.gameObject);
+			print ("Player 2 Wins!");
+		}
+
+	}
+}
